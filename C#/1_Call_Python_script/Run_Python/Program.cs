eabc8fbcbc2bc7cdf68de9d9ca61b1fa609100c7
Owen Kellie-Smith <oks.modelling@gmail.com>
2026-03-25 09:17:41 +0000

    C# as Python runner

---
+++ C#/1_Call_Python_script/Run_Python/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+var process = new Process();
+process.StartInfo.FileName = "python3";
+process.StartInfo.Arguments = "run_model.py -mp assets/csv/MPF.csv -a assets/xls -n 30 -r 0.03 -agg sum_year -o output.csv";
+// Set the working directory to where your Python script lives
+process.StartInfo.WorkingDirectory = @"../../../";
+process.StartInfo.RedirectStandardOutput = true;
+process.StartInfo.UseShellExecute = false;
+
+process.Start();
+
+string output = process.StandardOutput.ReadToEnd();
+process.WaitForExit();
+
+Console.WriteLine(output);
